c8aad98ee4f403793abb4f17c0133ef7a35f02a5
Benjamin Miranda <benjamin@boldware.cl>
2023-08-31 08:55:57 -0400

    chore: initial project settings

---
+++ test-001/src/app/about/page.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Bienvenidos&nbsp;
+          <code className={styles.code}>About</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="/about"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            About <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </div>
+    </main>
+  )
+}
