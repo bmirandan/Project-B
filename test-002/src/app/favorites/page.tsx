f9d87c261694603a163eb0b162ebbcc77014454d
Benjamin Miranda <benjamin@boldware.cl>
2023-09-15 00:22:11 -0300

    chore: no fav, fav list and localstorage management

--- test-002/src/app/favorites/page.tsx
+++ test-002/src/app/favorites/page.tsx
@@ -1,3 +1,19 @@
-export default function page() {
-  return <div>page</div>;
+'use client';
+
+import { useEffect } from 'react';
+import NoFavorites from './NoFavorites.component';
+import { getFavorites } from '@/libs/localFavorites';
+import PokemonFavorites from './PokemonFavorites.component';
+
+export default function HomePage() {
+  const pokemons = getFavorites();
+  
+  useEffect(() => {
+  }, [pokemons]);
+
+  return (
+    <div className=" w-screen h-screen p-20">
+      {pokemons.length > 0 ? <PokemonFavorites pokemonIds={pokemons} /> : <NoFavorites />}
+    </div>
+  );
 }
