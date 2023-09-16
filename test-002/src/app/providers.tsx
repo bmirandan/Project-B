f0358e7544ebab26a8ada9c7b35c2501081535e0
Benjamin Miranda <benjamin@boldware.cl>
2023-09-15 22:49:17 -0300

    chore: fix background

--- test-002/src/app/providers.tsx
+++ test-002/src/app/providers.tsx
@@ -1,14 +1,12 @@
 // app/providers.tsx
-"use client";
+'use client';
 
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from '@nextui-org/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <main className="purple-dark text-foreground bg-background" >
-        {children}
-      </main>
+      <main className="purple-dark text-foreground bg-background h-screen">{children}</main>
     </NextUIProvider>
   );
 }
