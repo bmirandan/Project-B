ce1fe62d5e6f027e075f89a658d9bc9349f27fb0
Benjamin Miranda <benjamin@boldware.cl>
2023-09-17 12:50:34 -0300

    Update page.tsx limit

--- test-002/src/app/name/[name]/page.tsx
+++ test-002/src/app/name/[name]/page.tsx
@@ -4,7 +4,7 @@ import { PokemonDetail } from '../../../components/PokemonDetail';
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1010');
 
   const pokemons = data.results.map(({ name }: { name: unknown }) => ({ name }));
 
